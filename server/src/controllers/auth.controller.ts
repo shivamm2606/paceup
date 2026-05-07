8735a789c9aeda006cda134134ca2678ef23b732
Shivam <shivamm.2606@gmail.com>
2026-05-08 03:02:31 +0530

    fix: update cookie sameSite attribute

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -10,7 +10,7 @@ const cookieOptions: {
 } = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  sameSite: "lax",
   maxAge: 1000 * 60 * 60 * 24 * 7,
 };
 
