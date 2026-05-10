1f1dc571fae9a52b10b771746c0425fcfad17996
Shivam <shivamm.2606@gmail.com>
2026-05-10 18:19:02 +0530

    fix: update cookie sameSite attribute based on environment

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -10,7 +10,7 @@ const cookieOptions: {
 } = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   maxAge: 1000 * 60 * 60 * 24 * 7,
 };
 
