84569be3e46c11b74fcee839abb83abc5aefc513
Shivam <shivamm.2606@gmail.com>
2026-05-10 16:53:52 +0530

    fix: update mailer transport configuration to use secure settings

--- server/src/utils/mailer.ts
+++ server/src/utils/mailer.ts
@@ -1,17 +1,20 @@
-import nodemailer from "nodemailer";
+import nodemailer, { TransportOptions } from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    family: 4,
+  },
   connectionTimeout: 20000,
   greetingTimeout: 20000,
   socketTimeout: 20000,
-});
+} as TransportOptions);
 
 export const sendEmail = async (
   to: string,
